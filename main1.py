baabc9c254d41ace86517b0309c6dc2e862adc6c
Rezo xaratishvili <rezo.xaratishvili@gmail.com>
2024-04-10 08:26:32 -0800

    first commit

---
+++ main1.py
@@ -0,0 +1,72 @@
+
+# დავალება 1
+# --------------------------------------------------------------------------------
+
+# number1 = int(input("შემოიყვანეთ რიცხვი: "))
+# number2 = int(input("შემოიყვანეთ რიცხვი: "))
+# number3 = int(input("შემოიყვანეთ რიცხვი: "))
+
+# print(number1 + number2 + number3)
+
+
+
+
+# დავალება 2
+# ---------------------------------------------------------------------------------
+
+# number = int(input("შემოიყვანეთ ცელსიუსი: "))
+# number2 = (number * 9/5) + 32
+
+# print("შემოტანილი რიცხვი ფარენჰეიტში არის: ", number2)
+
+
+
+
+# დავალება 3
+# ---------------------------------------------------------------------------------
+
+# length = float(input("ჩაწერეთ კუბის გვერდის სიგრძე: "))
+# surface = 6 * length ** 2
+# volume = length ** 3
+
+# print("კუბის ზედაპირის ფართობი: ", surface)
+# print("კუბის მოცულობა: ", volume)
+
+
+
+
+
+
+# starter = 500
+# print("საწყისი თანხა: ", starter)
+# money = float(input("შეიყვანეთ თანხა რამდენის გატანაც გსურს: "))
+# commission = 1
+# final = starter - (commission + money)
+
+# if final >= 0:
+#     print ("ბალანსზე არის დარჩენილი: ", final)
+# elif final < 0:
+#     print ("ბალანსზე არასაკმარისი თანხაა")
+
+
+
+
+
+
+# number1 = float(input("შემოიტანეთ პირველი რიცხვი: "))
+# number2 = float(input("შემოიტანეთ მეორე რიცხვი: "))
+# operator = input("შემოიტანეთ მათემატიკური ოპერატორი: ")
+
+# if operator == '+':
+#     print(number1 + number2)
+# elif operator == '-':
+#     print(number1 - number2)
+# elif operator == '*':
+#     print(number1 * number2)
+# elif operator == '/':
+#     print(number1 / number2)
+# else:
+#     print("შეცდომა")
+
+
+
